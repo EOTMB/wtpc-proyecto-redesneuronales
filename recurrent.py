753254798e12ead1d1c42d45fd0d2ba6246ab1c9
Tatotavella <alumno@inforcytb14.(none)>
2018-03-07 16:28:28 +0100

    Red recurrente con mayor volumen de datos

--- recurrent.py
+++ recurrent.py
@@ -6,27 +6,25 @@ from keras.layers import SimpleRNN, Dense
 import matplotlib.pyplot as plt
 
 ##
-# Data set simple
+# Data Set
 ##
 
-zer = np.zeros(5)
-uno = np.ones(5)
-set_dat = np.concatenate((zer,uno,zer,zer,zer))
-reset_dat = np.concatenate((zer,zer,zer,uno,zer))
-output_dat = np.concatenate((zer,uno,uno,zer,zer))
-
+set_dat = np.loadtxt('Valores_de_s.csv')
+reset_dat = np.loadtxt('Valores_de_r.csv')
+output_dat = np.loadtxt('Valores_de_o.csv')
+'''
 # Plot triple para visualizar
 f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
-ax1.plot(set_dat, '-o', color = 'green', label = 'set')
-ax2.plot(reset_dat, '-o', color = 'blue', label = 'reset')
-ax3.plot(output_dat, '-o', color = 'red', label = 'output')
+ax1.plot(set_dat[0:1000], '-o', color = 'green', label = 'set')
+ax2.plot(reset_dat[0:1000], '-o', color = 'blue', label = 'reset')
+ax3.plot(output_dat[0:1000], '-o', color = 'red', label = 'output')
 ax1.legend(numpoints = 1)
 ax2.legend(numpoints = 1)
 ax3.legend(numpoints = 1)
 plt.ylim([-0.1,1.1])
 plt.title('Batch simple')
 plt.show()
-
+'''
 ##
 # Parametros de la red
 # dim_inp es el tamano de inputs de la red
@@ -35,7 +33,7 @@ plt.show()
 ##
 
 dim_inp = 2
-dim_rnn = 50
+units_rnn = 50
 dim_out = 1
 
 ##
@@ -45,51 +43,96 @@ dim_out = 1
 # following batch.
 ##
 
+##
+# ERROR QUE TIRA
+# UserWarning: RNN dropout is no longer supported with the Theano 
+# backend due to technical limitations. You can either set 
+# `dropout` and `recurrent_dropout` to 0, or use the TensorFlow 
+# backend. RNN dropout is no longer supported with the Theano backend 
+##
+
 model = Sequential()
-model.add(SimpleRNN(input_dim = 2,
-                    units = 50,
+model.add(SimpleRNN(input_dim = dim_inp,
+                    units = units_rnn,
                     stateful = False,
                    # batch_input_shape = (25,10,2),
+                    dropout = 0,
+                    recurrent_dropout = 0,
                     return_sequences = True))
 
 # Que activacion usar ??
-model.add(Dense(units = 1, activation = "sigmoid"))
+model.add(Dense(units = dim_out, activation = "sigmoid"))
 
 
 ##
 # Formato de input de la red. CUIDADO, las redes neuronales recursivas se manejan con
-# "timesteps", con lo cual la data es separada en batches
+# "timesteps", con lo cual la data es separada en pedazos. Ver el siguiente link:
 # https://stackoverflow.com/questions/38294046/simple-recurrent-neural-network-input-shape
+# 
 ##
 
+##
+# Hay que pensar en "seeds" de data y que le son entregados a la red en "timesteps"
+# Shape del input de la red, es decir, de x_train: (largo_seed, cantidad_de_timesteps, dimension_de_input)
+# Notar que aca, por ejemplo, como tenemos input de dimension 2 y el largo de la seed es de 25 vamos a tener 
+# guardados arrays de shape (2, 25)
+##
 
-# Shape (2,data_length)
-sample_size = 1000
-in_list = np.array([set_dat, reset_dat])
-print(np.shape(in_list))
-x_train = np.array([[in_list] * sample_size]).reshape(len(set_dat),sample_size,2)
-print(np.shape(x_train))
-y_train = np.array([[output_dat] * sample_size]).reshape(len(output_dat),sample_size,1)
-print(np.shape(y_train))
+largo_seed = 100000
+cantidad_de_timesteps = 10
+entrada_array = np.zeros((largo_seed, cantidad_de_timesteps, dim_inp))
+salida_array = np.zeros((largo_seed, cantidad_de_timesteps, dim_out))
+print('entrada_array shape '+str(np.shape(entrada_array)))
+for i in range(cantidad_de_timesteps):
+    set_seed = set_dat[i * largo_seed : i * largo_seed + largo_seed]
+    reset_seed = reset_dat[i : i + largo_seed]
+    entrada_array[:,i,0] = set_seed
+    entrada_array[:,i,1] = reset_seed
+    out_seed = output_dat[i : i + largo_seed]
+    salida_array[:,i,0] = out_seed
+
+
+x_train = entrada_array
+y_train = salida_array
+
+print('------------------------------------------------------------------')
+print('Formatos de los datos de entrada a la red y al fit')
+print('Shape array de entrada: ' + str(np.shape(entrada_array)))
+print('Shape datos de entrada del fit: ' + str(np.shape(x_train)))
+print('Shape array de salida: ' + str(np.shape(salida_array)))
+print('Shape datos de salida del fit: ' + str(np.shape(y_train)))
+print('------------------------------------------------------------------')
 
 # Paso de compilacion
 # For a mean squared error regression problem
-#model.compile(optimizer='rmsprop',
-#              loss='mse')
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.compile(optimizer='rmsprop',
+#               loss='mse')
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_accuracy'])
 
-# Train the model, iterating on the data in batches of 32 samples
+# Train the model, iterating on the data in batches
 history = model.fit(x_train, y_train,
-                    epochs=100, batch_size = 100)
+                    epochs=5, batch_size = 50)
 
-test_in =  np.array([[in_list] * 1]).reshape(len(set_dat),1,2)
+
+# Test simple
+test_s = set_dat[0:1000]
+test_r = reset_dat[0:1000]
+test_o = output_dat[0:1000]
+test_in = np.zeros((1000,1,2))
+test_in[:,0,0] = test_s
+test_in[:,0,1] = test_r
+test_out = np.zeros((1000,1,1))
+test_out[:,0,0] = test_o
+
 out = np.array(model.predict(test_in))
 out = out[:,0,0]
-f, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=False)
-ax1.plot(output_dat, '-o', color = 'red', label = 'output real')
+f, (ax1, ax2) = plt.subplots(2, sharex = True, sharey = False)
+ax1.plot(test_o, '-o', color = 'red', label = 'output real')
 ax2.plot(out, '-o', color = 'blue', label = 'output red')
 ax1.legend(numpoints = 1)
 ax2.legend(numpoints = 1)
 plt.ylim([-0.1,1.1])
+
+plt.savefig('simple_test.png')
 plt.show()
 
